f14cfd87ca7830fc2cc9e92ee4ff961f7c360001
NammHai <namhaitranvu2005@gmail.com>
2024-11-11 04:54:57 +0700

    Add files via upload

---
+++ KoiDelivery.Services/Interfaces/IKhachHangService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiDelivery.Repositories.Entities;
+namespace KoiDelivery.Services.Interfaces
+{
+    public interface IKhachHangService
+    {
+        Task<List<TaiKhoanKhachHang>> TaiKhoanKhachHangs();
+        Boolean DelTaiKhoanKhachHang(int Id);
+        Boolean DelTaiKhoanKhachHang(TaiKhoanKhachHang account);
+        Boolean AddTaiKhoanKhachHang(TaiKhoanKhachHang account);
+        Boolean UpdTaiKhoanKhachHang(TaiKhoanKhachHang acocunt);
+        Task<TaiKhoanKhachHang> GetTaiKhoanKhachHangById(int Id);
+    }
+}
