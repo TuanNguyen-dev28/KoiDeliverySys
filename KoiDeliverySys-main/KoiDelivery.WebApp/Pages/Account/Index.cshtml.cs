a489108c14f6f2e79d15149bea6556c0eedd0449
NguyenLeDaiPhi <nguyenledaiphi0252005@gmail.com>
2024-11-10 21:48:38 +0700

    Folder đăng nhập
    Đăng nhập khách hàng

---
+++ KoiDeliverySys-main/KoiDelivery.WebApp/Pages/Account/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiDelivery.Repositories.Entities;
+using KoiDelivery.Services.Interfaces;
+
+namespace KoiDelivery.WebApp.Pages.Account
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IKhachHangService _service;
+        public IndexModel(IKhachHangService service)
+        {
+            _service = service;
+        }
+
+        public IList<TaiKhoanKhachHang> TaiKhoanKhachHang { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            TaiKhoanKhachHang = await _service.TaiKhoanKhachHangs();
+        }
+    }
+}
